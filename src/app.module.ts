2ff256626f682138d0ce0662b0605ec53de5599b
ron212job <ron212.job@gmail.com>
2022-03-18 00:16:12 +0200

    add modules to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 import config from "./config/keys"
 
 @Module({
-  imports: [TodosModule, MongooseModule.forRoot(config.mongoURI)],
+  imports: [TodosModule, MongooseModule.forRoot(config.mongoURI), UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
