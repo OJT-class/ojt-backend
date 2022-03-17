0c52a8daccf164a815e306ae91c05b10de5359fd
ron212job <ron212.job@gmail.com>
2022-03-18 00:52:00 +0200

    fixing imports in user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,6 +3,9 @@ import { InjectModel } from '@nestjs/mongoose';
 import { User } from './interfaces/user';
 import { Model } from 'mongoose';
 import { RegisterDTO } from './dto/register.dto';
+import { Payload } from './interfaces/payload';
+import * as bcrypt from 'bcrypt';
+import { LoginDTO } from 'src/auth/dto/login.dto';
 
 @Injectable()
 export class UserService {
@@ -21,10 +24,28 @@ export class UserService {
         return this.cleanUser(createdUser);
     }
 
+    async findByLogin(UserDTO: LoginDTO) {
+        const { email, password } = UserDTO;
+        const user = await this.userModel.findOne({ email });
+        if (!user) {
+          throw new HttpException('user doesnt exists', HttpStatus.BAD_REQUEST);
+        }
+        if (await bcrypt.compare(password, user.password)) {
+          return this.cleanUser(user)
+        } else {
+          throw new HttpException('invalid credential', HttpStatus.BAD_REQUEST);
+        }
+      }
+
     // clean password from user obj
     cleanUser(user: User) {
         const sanitizedUser = user.toObject();
         delete sanitizedUser['password'];
         return sanitizedUser;
+      }
+
+      async findByPayload(payload: Payload) {
+        const { email } = payload;
+        return await this.userModel.findOne({ email });
       }
 }
